163f7e78120d973efae03325f45a8c2abaa970e0
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-23 18:35:57 +0530

    feat: created a responsive sidebar for app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './components/Header'
+import { Link } from 'react-router-dom'
 
 function App() {
   
@@ -9,6 +10,19 @@ function App() {
   return (
     <>
      <Header text={"CRM Dashboard"}/>
+     <main className='container'>
+<div className='page-display'>
+  <div className='sidebar'>
+    <h2 className='sidebar-text'>Sidebar</h2>
+<Link className='btn-primary'>Leads</Link>
+<Link className='btn-primary'>Sales</Link>
+<Link className='btn-primary'>Agents</Link>
+<Link className='btn-primary'>Reports</Link>
+<Link className='btn-primary'>Settings</Link>
+  </div>
+  <div className='conten'></div>
+</div>
+     </main>
     </>
   )
 }
