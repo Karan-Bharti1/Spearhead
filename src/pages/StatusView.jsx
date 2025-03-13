50854b3e0db6e7a7bdf8a4927f1dab0534c30606
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-13 12:12:52 +0530

    feat: added filters to sort the data by salesAgent and priority along with it gave sort options to sort the data based on timeToClose

--- src/pages/StatusView.jsx
+++ src/pages/StatusView.jsx
@@ -3,12 +3,16 @@ import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchQueryStringBasedLeadsData } from "../features/leads/leadSlice";
+import { fetchSales } from "../features/salesAgents/SalesSlice";
 const StatusView=()=>{
     const dispatch=useDispatch()
     const leads=useSelector(state=>state.leads)
     const [data,setData]=useState([])
     const [currentStatus,setCurrentStatus]=useState("")
-
+    const sales=useSelector(state=>state.sales)
+    const [salesPerson,setSalesPerson]=useState("")
+    const [selectedPriority,setSelectedPriority]=useState("")
+    const [timeSort, setTimeSort] = useState("")
     const handleChange = (e) => {
         const status = e.target.value;
         setCurrentStatus(status);
@@ -20,10 +24,28 @@ const StatusView=()=>{
             }));
         }
     };
+    useEffect(()=>{
+dispatch(fetchSales())
+    },[dispatch])
    useEffect(()=>{
     setData(leads.leads)
    },[leads])
+   console.log(data)
+const filteredData=data?.filter(lead=>{
+    const matchesSales=salesPerson===""||lead.salesAgent._id===salesPerson||salesPerson=="All"
+    const matchesPriority=selectedPriority===""||selectedPriority==="All"||lead.priority===selectedPriority
+    return matchesSales && matchesPriority
+})
+if(timeSort==="highToLowTime"){
+       
+   filteredData.sort((a, b) => b.timeToClose - a.timeToClose)
 
+}
+if(timeSort==="lowToHighTime"){
+    
+  filteredData.sort((a, b) => a.timeToClose - b.timeToClose)
+
+}
     return(<>
     <Header text={"Lead Status View"}/>
     <main className="container">
@@ -35,7 +57,7 @@ const StatusView=()=>{
        
         <div className="content">
             <div>
-            <h3>Select a status to see leads for that status:{" "}
+            <h3>Select a status:{" "}
 
             
             <select onChange={handleChange}>
@@ -48,20 +70,53 @@ const StatusView=()=>{
             </select>
             </h3>
             <hr/>
-            <h3 className="sec-heading">{currentStatus}</h3>
+            
             {leads.status==="loading" && <h2 className="load">Loading...</h2>}
-            {leads.status!=="loading"&& currentStatus && data.length > 0 && (
+            {leads.status!=="loading"&& currentStatus && data.length > 0 && (<>
+                <h3 className="sec-heading">Status: {currentStatus}</h3>
+                <div className="filteredLeadsByStatus">
+                <div>
+                <label><strong>Filters: </strong></label>{" "}
+                <select onChange={event=>setSalesPerson(event.target.value)}>
+<option value="">Select Sales Agent</option>
+<option value="All">All</option>
+{sales?.sales?.map(sale=>(<option key={sale._id} value={sale._id}>{sale.name}</option>))}
+
+</select>{" "}
+<select onChange={event=>setSelectedPriority(event.target.value)}>
+    <option value="">Select Priority</option>
+    <option value="All">All</option>
+    <option value="High">High</option>
+    <option value="Medium">Medium</option>
+    <option value="Low">Low</option>
+</select>
+</div>
+<div>
+<label><strong>Sort By: </strong></label>
+<select onChange={event=>setTimeSort(event.target.value)}>
+    <option value="">Sort by Time To Close</option>
+    <option value="highToLowTime">High to Low Close Time</option>
+    <option value="lowToHighTime">Low to High Close Time</option>
+</select>
+</div>
+
+
+
+
+</div>
+
                                     <ul>
-                                        {data.map(lead => (
-                                            <li key={lead._id } className="leadList">
-                                                {lead.name} 
-                                            </li>
+                                        {filteredData.map(lead => (
+                                            <li key={lead._id} className="leadList"><span ><Link to={`/viewdetails/${lead._id}`}>{lead.name} [{lead.status}]</Link></span>
+<span className="comment-text">~{lead.salesAgent.name}</span></li>
+                                           
                                         ))}
-                                    </ul>
+                                        {filteredData.length===0 && <p className="sec-heading">No leads Found</p>}
+                                    </ul></>
                                 ) }
                                 {leads.status!=="loading"&& currentStatus &&  data.length == 0 && <p className="sec-heading">No Leads Found</p>}
                                 <br></br>
-                             {   !currentStatus && <p className="sec-heading">You need to select any status value to view leads data for that stataus</p>}
+                             {   !currentStatus && <p className="sec-heading">You need to select any status value to view leads data for that status.</p>}
             </div>
             </div>
             </div>
